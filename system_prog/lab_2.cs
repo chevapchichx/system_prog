ddde8fa2aa10d8bf03c360edf9008c395ea8085c
chevapchich pc <tugaisveta@gmail.com>
2025-04-03 17:34:28 +0300

    j

--- system_prog/lab_2.cs
+++ system_prog/lab_2.cs
@@ -6,8 +6,8 @@ public class Lab2
 {
     public static void Lab_2()
     {
-        Console.WriteLine("hello");
-
+        HANDLE file_lab_2 = CreateFile("lab_2.txt", GENERIC_READ | GENERIC_WRITE, 0, null, OPEN_EXISTING, 0, 0);
+        Console.WriteLine("descriptor: {0}", file_lab_2);
     }
 
 }
--- system_prog/obj/Debug/net9.0/system_prog.AssemblyInfo.cs
+++ system_prog/obj/Debug/net9.0/system_prog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("system_prog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da6c3324b1695d670fb5b3fb01f15a97128a48dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aee9191e10fa4b59f81975eb597e1cd61080587")]
 [assembly: System.Reflection.AssemblyProductAttribute("system_prog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("system_prog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

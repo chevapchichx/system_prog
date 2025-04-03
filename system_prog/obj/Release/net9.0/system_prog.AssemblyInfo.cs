da6c3324b1695d670fb5b3fb01f15a97128a48dd
chevapchich pc <tugaisveta@gmail.com>
2025-04-03 13:42:37 +0300

    hz3

--- system_prog/obj/Release/net9.0/system_prog.AssemblyInfo.cs
+++ system_prog/obj/Release/net9.0/system_prog.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("system_prog")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21f7ded6df6642a51f8e7ff22c08ac56113b843a")]
 [assembly: System.Reflection.AssemblyProductAttribute("system_prog")]
